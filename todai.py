fe180ef124eeb64c4efa1ced23133cfa515f6c44
soradacroi <soradacroi@gmail.com>
2025-11-02 18:58:29 +0530

    save sys

--- todai.py
+++ todai.py
@@ -1,5 +1,15 @@
 import tkinter as tk
 
+from datetime import date
+
+import os
+
+if not os.path.exists("dairy"):
+    os.makedirs("dairy")
+
+today = str(date.today())
+full_file_path = os.path.join("dairy", today)
+
 root = tk.Tk() 
 
 # --- WINDOW SETUP ---
@@ -9,32 +19,12 @@ root.configure(bg="#9B7E69")
 
 def save_button_c():
     content = writing_area.get("1.0", tk.END + "-1c")
-    print("--- Text Content ---")
-    print(content)
+    with open(f'{full_file_path}', 'w') as page:
+        page.write(content)
 
 root.grid_rowconfigure(0, weight=1) 
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=0)
-
-# --- WRITING AREA (THE "PAPER") ---
-writing_area = tk.Text(
-    root,
-    wrap=tk.WORD,
-    font=("Times New Roman", 14),
-    bg="#F5E9D5", 
-    fg="#2A2928", 
-    bd=5,
-    relief=tk.SUNKEN
-)
-
-writing_area.grid(
-    row=0, 
-    column=0, 
-    rowspan=2,
-    padx=(20, 5),
-    pady=(20, 20), 
-    sticky='nsew'
-)
 
 
 control_frame = tk.Frame(root, bg="#9B7E69")
@@ -51,39 +41,86 @@ control_frame.grid(
 
 control_frame.grid_rowconfigure(100, weight=1)
 
+try:
+    with open(r'last_date', 'r') as ld:
+        if ld.read() != f"{today}":
+            save = tk.Button(
+                control_frame, 
+                text="     save     ", 
+                command=save_button_c,
+                bg="#8B4513",
+                fg="#FFF8DC",
+                font=('Times New Roman', 12, 'bold')
+            )
 
-hello_label = tk.Label(
-    control_frame,
-    text="hello",
-    bg="#9B7E69",
-    fg="#F5E9D5",
-    font=('Times New Roman', 14, 'bold')
-)
+            save.grid(
+                row=99,
+                column=0, 
+                padx=5,
+                pady=10,
+                sticky='s'
+            )
 
-hello_label.grid(
-    row=0,
-    column=0,
-    padx=5,
-    pady=(0, 10),
-    sticky='n'
-)
+            writing_area = tk.Text(
+                root,
+                wrap=tk.WORD,
+                font=("Times New Roman", 14),
+                bg="#F5E9D5", 
+                fg="#2A2928", 
+                bd=5,
+                relief=tk.SUNKEN
+            )
 
+            writing_area.grid(
+                row=0, 
+                column=0, 
+                rowspan=2,
+                padx=(20, 5),
+                pady=(20, 20), 
+                sticky='nsew'
+            )
+        else:
+            _all = tk.Label(root, text="YOU HAVE ALREAD WROTE TODAY")
+            _all.grid(row=0, column=0, padx=5, pady=5)
 
-my_button = tk.Button(
-    control_frame, 
-    text="     save     ", 
-    command=save_button_c,
-    bg="#8B4513",
-    fg="#FFF8DC",
-    font=('Times New Roman', 12, 'bold')
-)
+except:
+    with open(r'last_date', 'w') as ld:
+        ld.write(today)
+        save = tk.Button(
+                control_frame, 
+                text="     save     ", 
+                command=save_button_c,
+                bg="#8B4513",
+                fg="#FFF8DC",
+                font=('Times New Roman', 12, 'bold')
+            )
 
-my_button.grid(
-    row=99,
-    column=0, 
-    padx=5,
-    pady=10,
-    sticky='s'
-)
+        save.grid(
+                row=99,
+                column=0, 
+                padx=5,
+                pady=10,
+                sticky='s'
+            )
+
+        writing_area = tk.Text(
+                root,
+                wrap=tk.WORD,
+                font=("Times New Roman", 14),
+                bg="#F5E9D5", 
+                fg="#2A2928", 
+                bd=5,
+                relief=tk.SUNKEN
+            )
+
+        writing_area.grid(
+                row=0, 
+                column=0, 
+                rowspan=2,
+                padx=(20, 5),
+                pady=(20, 20), 
+                sticky='nsew'
+            )
+
 
 root.mainloop()
